bd3bc9a81439cb5cb93b214a0ef644775cdfa49a
al222210660@gmail.com <tuemail@example.com>
2025-04-12 13:35:34 -0600

    A

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,7 +7,7 @@ const Login = ({ onLogin }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:5000/api/login', {
+    const response = await fetch('http://3.145.101.190/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
